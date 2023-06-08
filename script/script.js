b9b157b3a0df27cd589bfdf1072bcfdbad91387d
Miguel Urena <urena_miguel82@outlook.com>
2023-06-07 17:14:08 -0700

    Fix a bug

--- script/script.js
+++ script/script.js
@@ -2,9 +2,10 @@
 const header = document.querySelector('.navbar');
 
 window.onscroll = function() {
-  if (top >= 100) {
+  var top = window.scrollY;
+  if(top >= 100) {
     header.classList.add('navbarDark');
   } else {
     header.classList.remove('navbarDark');
   }
-};
+}
