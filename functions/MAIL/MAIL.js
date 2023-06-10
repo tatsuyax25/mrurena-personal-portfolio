c265ecc6f817f1e52895afd34a62ffd781151860
Miguel Urena <urena_miguel82@outlook.com>
2023-06-10 14:39:19 -0700

    Fixing email

--- functions/MAIL/MAIL.js
+++ functions/MAIL/MAIL.js
@@ -5,7 +5,7 @@ exports.handler = async (event, context) => {
 
   const data = JSON.parse(event.body);
   console.log('Data:', data);
-  
+
   const transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
@@ -23,11 +23,13 @@ exports.handler = async (event, context) => {
 
   try {
     await transporter.sendMail(mailOptions);
+    console.log('Email sent');
     return {
       statusCode: 200,
-      body: "Email sent"
+      body: JSON.stringify({ message: "Email sent" })
     }
   } catch (error) {
+    console.error('Error sending email:', error);
     return {
       statusCode: error.statusCode || 500,
       body: JSON.stringify({ message: error.message })
